ded5b6fdbf89753fab6d3ebd71ef0677f4445eca
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 12:15:01 +0000

    Ex 10: Made a sort of piramid based on the user input on the args using for loops

---
+++ Triangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Triangle
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            int a = int.Parse(args[0]);
+            char b = char.Parse(args[1]);
+
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.WriteLine(b);
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
