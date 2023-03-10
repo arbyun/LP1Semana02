36586b52cbe089aa1ce3714a000581eb6e656cf4
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 11:00:33 +0000

    Ex 04: asked user for cylinder's height (h)

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Let's build a cylinder.");
+            Console.WriteLine("Please, input it's height:");
+            
+            double h = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            
+        }
+    }
+}
