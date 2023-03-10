a215f30421f738ce64cf7a81c572ae93ed83c8ac
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 11:25:17 +0000

    Ex 06: Added all max values

---
+++ Specials/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Specials
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            double db = double.MaxValue;
+            float ft = float.MaxValue;
+            int i = int.MaxValue;
+            sbyte sb = sbyte.MaxValue;
+            short shortish = short.MinValue;
+            long longish = long.MaxValue;
+            ushort shorter = ushort.MaxValue;
+            byte bt = byte.MaxValue;
+            ulong longer = ulong.MaxValue;
+            uint ui = uint.MaxValue;
+
+        }
+    }
+}
