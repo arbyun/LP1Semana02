c203d4bd955f2eb0cf1734e8f5fce5f134439b1f
Daniela Castro <someonealiveh@gmail.com>
2023-03-10 11:28:53 +0000

    Ex 06: Added all min values

--- Specials/Program.cs
+++ Specials/Program.cs
@@ -8,12 +8,25 @@
             float ft = float.MaxValue;
             int i = int.MaxValue;
             sbyte sb = sbyte.MaxValue;
-            short shortish = short.MinValue;
+            short shortish = short.MaxValue;
             long longish = long.MaxValue;
             ushort shorter = ushort.MaxValue;
             byte bt = byte.MaxValue;
             ulong longer = ulong.MaxValue;
             uint ui = uint.MaxValue;
+            
+            double sdb = double.MinValue;
+            float sft = float.MinValue;
+            int si = int.MinValue;
+            sbyte ssb = sbyte.MinValue;
+            short sshortish = short.MinValue;
+            long slongish = long.MinValue;
+            ushort sshorter = ushort.MinValue;
+            byte sbt = byte.MinValue;
+            ulong slonger = ulong.MinValue;
+            uint sui = uint.MinValue;
+            
+            
 
         }
     }
